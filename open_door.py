13547996d50027cb8541256e1130eedb3f055159
dpeddi <1086815+dpeddi@users.noreply.github.com>
2023-11-26 21:34:10 +0100

    add all available command retrieved from the app

--- open_door.py
+++ open_door.py
@@ -37,11 +37,99 @@ class CgiError(Enum):
     CGI_ERROR_NOAUTH = -45
     CGI_ERROR_NOSPARINGTIME = -46
 
+COMMAND_GET_ALARM_CRYDETECTION = "get.alarm.crydetection"
+COMMAND_GET_ALARM_HUMANDETECTION = "get.alarm. humandetection"
+COMMAND_GET_ALARM_INPUT = "get.alarm.alarmin"
+COMMAND_GET_ALARM_MOTION_DETECTION = "get.alarm.motiondetection"
+COMMAND_GET_ALARM_VIDEO_LOST = "get.alarm.videolost"
+COMMAND_GET_ALARM_VIDEO_SHELTER = "get.alarm.videoshelter"
+COMMAND_GET_ATTACHMENT_INFO = "get.device.attachInfo"
+COMMAND_GET_DEVICE_ALARM_ALARMIN_SCHEDULE = "get.alarm.alarmin.schedule"
+COMMAND_GET_DEVICE_ALARM_CHANNELLIST = "get.encode.channelname"
+COMMAND_GET_DEVICE_ALARM_MOTIONDETECTION_SCHEDULE = "get.alarm.motiondetection.schedule"
+COMMAND_GET_DEVICE_ALARM_VIDEOLOST_SCHEDULE = "get.alarm.videolost.schedule"
+COMMAND_GET_DEVICE_ALARM_VIDEOSHELTER_SCHEDULE = "get.alarm.videoshelter.schedule"
+COMMAND_GET_DEVICE_ALL_INFO = "get.device.status" # ok
+COMMAND_GET_DEVICE_INFO = "get.product.info" # ok
+COMMAND_GET_DEVICE_LATEST_VERSION = "get.system.upgradeversion" # ok
+COMMAND_GET_DEVICE_SECRET = "get.device.streamkey"
+COMMAND_GET_DEVICE_UPGRADE_PROCESS = "get.system.upgradeprocess" # ok
+COMMAND_GET_DEVICE_UPGRADE_STATUS = "get.system.upgradestatus" # ok
+COMMAND_GET_FPS = "get.encode.fps"
+COMMAND_GET_HUMANTRACE_INFO = "get.humantrace.info"
+COMMAND_GET_MOTION_DETECTION = "get.motiondetection.info"
+COMMAND_GET_MOVEDETECTION_INFO = "get.movedetection.info"
+COMMAND_GET_NETWORK_BASE = "get.network.base"
+COMMAND_GET_NETWORK_CONFIG = "get.network.config"
+COMMAND_GET_NETWORK_WORKMODE = "get.network.workmode"
+COMMAND_GET_PAN_TILT_CONTROL_POSITION = "get.ptz.position"
+COMMAND_GET_PTZ_PRESET = "get.ptz.preset"
+COMMAND_GET_QR_CODE = "get.device.qrcode" # ok
+COMMAND_GET_RECORD_ALARM_INFO = "get.record.alarmrecord"
+COMMAND_GET_RECORD_CONFIG = "get.record.config"
+COMMAND_GET_RECORD_MESSAGE = "get.record.message"
+COMMAND_GET_RECORD_SESSION = "get.record.session"
+COMMAND_GET_SHAPE_MIRROR = "get.shape.mirror"
+COMMAND_GET_SMART_LIGHT_INFO = "get.smart.lightinfo"
+COMMAND_GET_SOUNDANDLIGHT_STATE = "get.soundandlight.state"
+COMMAND_GET_STORAGE_INFO = "get.hdd.base"
+COMMAND_GET_SYSTEM_ABILITY = "get.system.ability"
+COMMAND_GET_SYSTEM_GENERAL = "get.system.general"
+COMMAND_GET_TF_CARD_INFO = "get.tfcard.info" # ok
+COMMAND_GET_TIME_ZONE = "get.product.time" # ok
+COMMAND_GET_VIDEO_CONFIG_BITRATELIST_INFO = "get.encode.bitratelist"
+COMMAND_GET_VIDEO_CONFIG_INFO = "get.encode"
+COMMAND_GET_VIDEO_SWITCH = "get.videoswitch.vionoff"
+COMMAND_GET_VIDEO_TIME_TITLE = "get.video.timetitle"
+COMMAND_GET_WIFI_LIST = "get.wifi.list"
+COMMAND_CHECK_UNLOCK_PASSWORD = "set.opendoor.checkpassword"
+COMMAND_GET_DEVICE_UNLOCK = "set.device.opendoor"
+COMMAND_MODIFY_VERIFICATION_CODE = "set.seurity.verifycode"
+COMMAND_OPEN_LOCK = "set.device.opendoor"
+COMMAND_SET_ALARM_CRYDETECTION = "set.alarm.crydetection"
+COMMAND_SET_ALARM_HUMANDETECTION = "set.alarm.humandetection"
+COMMAND_SET_ALARM_INPUT = "set.alarm.alarmin"
+COMMAND_SET_ALARM_MOTION_DETECTION = "set.alarm.motiondetection"
+COMMAND_SET_ALARM_VIDEO_LOST = "set.alarm.videolost"
+COMMAND_SET_ALARM_VIDEO_SHELTER = "set.alarm.videoshelter"
+COMMAND_SET_DEBUG_SYNC_TIME = "set.debug.synctime"
+COMMAND_SET_DEVICE_ALARM_ALARMIN_SCHEDULE = "set.alarm.alarmin.schedule"
+COMMAND_SET_DEVICE_ALARM_CHANNELLIST = "set.encode.channelname"
+COMMAND_SET_DEVICE_ALARM_MOTIONDETECTION_SCHEDULE = "set.alarm.motiondetection.schedule"
+COMMAND_SET_DEVICE_ALARM_VIDEOLOST_SCHEDULE = "set.alarm.videolost.schedule"
+COMMAND_SET_DEVICE_ALARM_VIDEOSHELTER_SCHEDULE = "set.alarm.videoshelter.schedule"
+COMMAND_SET_DEVICE_UPGRADE = "set.system.upgrade"
+COMMAND_SET_F1_STATE = "set.device.f1function.enable"
+COMMAND_SET_FPS = "set.encode.fps"
+COMMAND_SET_HUMANTRACE_INFO = "set.humantrace.info"
+COMMAND_SET_MOTION_DETECTION = "set.motiondetection.info"
+COMMAND_SET_MOVEDETECTION_INFO = "set.movedetection.info"
+COMMAND_SET_NETWORK_BASE = "set.network.base"
+COMMAND_SET_NETWORK_CONFIG = "set.network.config"
+COMMAND_SET_NETWORK_WORKMODE = "set.network.workmode"
+COMMAND_SET_PAN_TILT_CONTROL_POSITION = "set.ptz.position"
+COMMAND_SET_PTZ_PRESET = "set.ptz.preset"
+COMMAND_SET_PTZ_PRESET_CLEAR = "set.ptz.preset.clear"
+COMMAND_SET_PTZ_PRESET_GOTO = "ctrl.ptz.preset.goto"
+COMMAND_SET_RECORD_CONFIG = "set.record.config"
+COMMAND_SET_SHAPE_MIRROR = "set.shape.mirror"
+COMMAND_SET_SMART_LIGHT_INFO = "set.smart.lightinfo"
+COMMAND_SET_SMART_LIGHT_MODE = "set.smart.lightmode"
+COMMAND_SET_SOUNDANDLIGHT_STATE = "set.soundandlight.state"
+COMMAND_SET_SUMMER_TIME = "set.system.summertime"
+COMMAND_SET_SYSTEM_GENERAL = "set.system.general"
+COMMAND_SET_TF_CARD_FORMAT = "set.tfcard.format"
+COMMAND_SET_TIME_ZONE = "set.product.time"
+COMMAND_SET_UNLOCK_PASSWORD = "set.opendoor.password"
+COMMAND_SET_VIDEO_CONFIG_INFO = "set.encode"
+COMMAND_SET_VIDEO_SWITCH = "set.videoswitch.vionoff"
+COMMAND_SET_WIFI_INFO = "set.wifi.info"
+
 m = hashlib.sha256()
 m.update(args.password.encode('utf-8'))
 passwordencode = m.hexdigest()
 
-def send_command(command, payload):
+def send_command(command, payload, debug = False):
 
     headers = {
       'Accept-Language': 'en-US,en;q=0.5',
@@ -62,7 +150,8 @@ def send_command(command, payload):
       #print ("posting", payload)
       response = session.post(url, data=payload, headers=headers, allow_redirects=True, verify=False)
       print ("STATUS_CODE:",response.status_code)
-#      print (response.text)
+      if debug:
+          print (response.text)
 
       doc = xmltodict.parse(response.text, process_namespaces=True)
 
@@ -81,7 +170,7 @@ if __name__ == "__main__":
 
   for t in TARGETS:
     url = f'http://{t}/tdkcgi'
-    command = "get.product.info"
+
     payload = "<?xml version='1.0' encoding='utf-8'?>\n<envelope><header><security>username</security><username>%s</username><password>%s</password></header><body><command>%s</command><content></content></body></envelope>" % (username, passwordencode, command)
     print ("SOFTWARE VERSION:",send_command(command, payload)["envelope"]["body"]["content"]["info"]["version"])
 
@@ -90,22 +179,9 @@ if __name__ == "__main__":
     print ("TIMEZONE:",send_command(command, payload)["envelope"]["body"]["content"]["time"]["timezone"])
 
 
-#    command = "get.system.login" ko
     command = "get.device.status"
-
     payload = "<?xml version='1.0' encoding='utf-8'?>\n<envelope><header><security>username</security><username>%s</username><password>%s</password></header><body><command>%s</command><content></content></body></envelope>" % (username, passwordencode, command)
     send_command(command, payload)
-
-
-#    command = "get.device.streamkey" ko
-#    command = "get.device.streamkey" ko
-#    command = "get.network.config"
-#    command = "get.network.base"
-#    command = "get.system.upgradestatus"
-#    command = "get.product.time"
-#    command = "get.videoswitch.vionoff" #ko
-#    command = "get.wifi.list" # ko
-#    payload = "<?xml version='1.0' encoding='utf-8'?>\n<envelope><header><security>username</security><username>%s</username><password>%s</password></header><body><command>%s</command><content></content></body></envelope>" % (username, passwordencode, command)
 
 #    command = "set.opendoor.checkpassword"
 #    payload = "<?xml version='1.0' encoding='utf-8'?>\n<envelope><header><security>username</security><username>%s</username><password>%s</password></header><body><command>%s</command><content><password>%s</password></content></body></envelope>" % (username, passwordencode, command, passwordencode)
